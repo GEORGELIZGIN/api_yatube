515f5d166be2fb23e525c2f06c9b65f22f78d64d
Georgiy <lizgin.gv@phystech.edu>
2021-05-14 23:23:41 +0300

    commit

---
+++ posts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return obj.author.username
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+        read_only_fields = ('author',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return obj.author.username
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('author', 'post')
--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+comments_router = DefaultRouter()
+posts_router = DefaultRouter()
+comments_router.register(r'(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+posts_router.register('posts', PostViewSet)
 
 urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
+    path('', include(posts_router.urls)),
+    path(r'posts/', include(comments_router.urls)),
 ]
